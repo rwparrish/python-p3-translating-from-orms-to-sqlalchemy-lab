a0109746d3e560984a2f96df14ed792b7dda7ef6
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-05-30 19:39:45 -0600

    UPDATE

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
+import ipdb
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    if __name__ == '__main__':
+        engine = create_engine('sqlite:///dogs.db')
+        Base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
+    # ipdb.set_trace()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.add(dog)
+    session.commit()
+  
+    
